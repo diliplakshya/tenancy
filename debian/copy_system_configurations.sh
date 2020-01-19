91b41e0a3c3fc4539b4f7d0ae9f31868a6ecbdd9
= <=>
2020-01-19 22:29:25 +1100

    added start up script

--- debian/copy_system_configurations.sh
+++ debian/copy_system_configurations.sh
@@ -7,4 +7,10 @@
 make_dirs $DEPLOY_ETC_INIT_D_DIR
 
 # Copy 'hotel_init' to '/etc/init.d/start_up.sh'
-copy_files $PROJECT_START_UP_DIR/hotel_init $DEPLOY_ETC_INIT_D_DIR/hotel_init
+copy_files $PROJECT_START_UP_DIR/hotel_init.sh $DEPLOY_ETC_INIT_D_DIR/hotel_init
+
+# Create etc init directory (/etc/init)
+make_dirs $DEPLOY_ETC_INIT_DIR
+
+# Copy 'hotel_start.conf' to '/etc/init/hotel_start.conf'
+copy_files $PROJECT_START_UP_DIR/hotel_init.conf $DEPLOY_ETC_INIT_DIR/hotel_init.conf
--- scripts/system/hotel_init.sh
+++ scripts/system/hotel_init.sh
@@ -2,7 +2,7 @@
 # chkconfig: 35 99 01
 # description: Starts and stops the Hotel Booking System
 
-# Run 'chkconfig --add start_up.sh' (as root) to enable system startup on boot
+# Run 'chkconfig --add hotel_init' (as root) to enable system startup on boot
 
 su - aspect /home/aspect/hotel_booking/bin/init/start.sh
 
