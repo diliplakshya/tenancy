0c0c90f1c562e01135ebd4fa281d07ea8a9b7404
Dilip Kumar Sharma <diliplakshya@gmail.com>
2020-01-20 16:27:25 +1100

    removed copying optional file

--- debian/copy_system_configurations.sh
+++ debian/copy_system_configurations.sh
@@ -12,5 +12,6 @@ copy_files $PROJECT_START_UP_DIR/hotel_init.sh $DEPLOY_ETC_INIT_D_DIR/hotel_init
 # Create etc init directory (/etc/init)
 make_dirs $DEPLOY_ETC_INIT_DIR
 
+# This is optional
 # Copy 'hotel_init.conf' to '/etc/init/hotel_init.conf'
-copy_files $PROJECT_START_UP_DIR/hotel_init.conf $DEPLOY_ETC_INIT_DIR/hotel_init.conf
+# copy_files $PROJECT_START_UP_DIR/hotel_init.conf $DEPLOY_ETC_INIT_DIR/hotel_init.conf
