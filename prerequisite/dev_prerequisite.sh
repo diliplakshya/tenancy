06e629db66fbabd8ba6f77bd24fd9b928440011c
Dilip Kumar Sharma <diliplakshya@gmail.com>
2020-02-02 13:16:33 +1100

    Added new source directories

--- prerequisite/dev_prerequisite.sh
+++ prerequisite/dev_prerequisite.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo "Setting Development machine ..."
+echo "****Installing packages for development machine, please wait****"
+echo "****Installing QT Creator****"
+apt-get --yes --force-yes install build-essential
+apt-get --yes --force-yes install qtcreator
+
+echo "****Installing QT5****"
+apt-get --yes --force-yes install qt5-default
 echo "**********************************"
