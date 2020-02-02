005ccca66ba5905cc5528ba7d74c0302555edc27
Dilip Kumar Sharma <diliplakshya@gmail.com>
2020-02-02 20:03:35 +1100

    added C python api source file

--- prerequisite/dev_prerequisite.sh
+++ prerequisite/dev_prerequisite.sh
@@ -9,3 +9,8 @@ apt-get --yes --force-yes install qtcreator
 echo "****Installing QT5****"
 apt-get --yes --force-yes install qt5-default
 echo "**********************************"
+
+echo "****Installing Python Dev Package****"
+# This is to get Python.h and Python C API (embedding Python into C++)
+apt-get --yes --force-yes install python3.5-dev
+echo "**********************************"
---
+++ src/cpp/lib/db_connection.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+# include <Python.h>
+
+
+int main()
+{
+    std::cout << "A CPP/Python library" << std::endl;
+    return 0;
+}
