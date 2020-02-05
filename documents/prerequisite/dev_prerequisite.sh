b997065b2f4d214e9272f43e1fcd3bb55f62dbcc
Dilip Kumar Sharma <diliplakshya@gmail.com>
2020-02-05 17:36:04 +1100

    Updated prequisites

--- documents/prerequisite/dev_prerequisite.sh
+++ documents/prerequisite/dev_prerequisite.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 echo "Setting Development machine ..."
-echo "****Installing packages for development machine, please wait****"
-echo "****Installing QT Creator****"
-apt-get --yes --force-yes install build-essential
-apt-get --yes --force-yes install qtcreator
+echo "**** Installing packages for development machine, please wait ****"
 
-echo "****Installing QT5****"
-apt-get --yes --force-yes install qt5-default
-echo "**********************************"
 
-echo "****Installing Python Dev Package****"
-# This is to get Python.h and Python C API (embedding Python into C++)
-apt-get --yes --force-yes install python3.5-dev
-echo "**********************************"
