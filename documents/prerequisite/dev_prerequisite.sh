6b270e33ce7a0c954ea2e09e6fbf5dc8d71994af
Dilip Kumar <diliplakshya@gmail.com>
2020-04-28 17:59:14 +1000

    modified prerequisites

--- documents/prerequisite/dev_prerequisite.sh
+++ documents/prerequisite/dev_prerequisite.sh
@@ -3,4 +3,7 @@
 echo "Setting Development machine ..."
 echo "**** Installing packages for development machine, please wait ****"
 
-
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install python3.7
+sudo apt-get -y install python3.7-dev
